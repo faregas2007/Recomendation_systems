f8ae54cd8ed027866162d16f84959cc9608d4dda
faregas2007 <faregas2007@gmail.com>
2021-07-24 00:27:26 +0200

    update files

--- mfpt.py
+++ mfpt.py
@@ -9,6 +9,27 @@ from scipy.sparse import rand as sprand
 import argparse import Namespace
 from typing import List
 
+def Interactions(data.Dataset):
+    """
+    Hold data in the form of an interactions matrix
+    Users are rows
+    Items are columns
+    Elements of the matrix are the ratings given by a user for an item
+    """
+    def __init__(self, mat):
+        self.mat = mat.astype(np.float32).tocoo()
+        self.n_users = self.mat.shape[0]
+        self.n_items = self.mat.shape[1]
+    
+    def __getitem__(self, index):
+        row = self.mat.row[index]
+        col = self.mat.col[index]
+        val = self.mat.data[index]
+        return (row, col), val
+
+    def __len__(self):
+        return self.mat.nnz
+
 class mfpt(nn.Module):
     def __init__(self, 
             n_users:int,
@@ -16,6 +37,21 @@ class mfpt(nn.Module):
             n_factors:int,
             dropout_p: float,
             sparse: bool)->None:
+        """
+        Parameters
+        ----------
+        n_users: int
+            Number of users
+        n_items: int
+            Number of items
+        n_factors: int
+            Number of latent factors (or embeddings or whatever you want to call it)
+        dropout_p : float
+            p in nn.Dropout module. Probability of dropout.
+        sparse: bool
+            Whether or not to treat embeddings as sparse. NOTE: cannot use weight decay
+            on the optimizer if sparse=True. Also, can only use Adagrad.
+        """
         super(mfpt, self).__init__()
 
         self.n_users = torch.tensor(n_users, dtype=float)
@@ -31,6 +67,25 @@ class mfpt(nn.Module):
         self.item_factors = torch.nn.Embedding(n_items, n_factors, sparse=sparse)
 
     def forward(self, user: int, item:int)-> torch.Tensor:
+        """
+        Forward pass through the model. For a single user and item, this
+        looks like:
+
+        user_bias + item_bias + user_embeddings.dot(item_embeddings)
+
+        Parameters
+        ----------
+        users: np.ndarray
+            Array of user indices
+        items: np.ndarray
+            Array of item indices
+
+        Returns 
+        -------
+        preds: np.ndarray
+            Predicted ratings
+        """
+        
         ues = self.user_factors(user)
         uis = self.item_factors(item)
 
@@ -39,6 +94,13 @@ class mfpt(nn.Module):
 
         preds += ((self.dropout(ues)*self.dropout(uis)).sum(dim=1, keepdim=True))
         return preds.squeeze()
+
+    def __call__(self, *args):
+        return self.forward(*args)
+
+    def predict(self, users, items):
+        return self.forward(users, items)
+
 
 def initialize_model(
     params: Namespace
