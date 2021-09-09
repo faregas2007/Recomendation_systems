6492e9c10762ca81465786cd7dd5cec47efd8fdc
dnguyen <faregas2007@gmail.com>
2021-09-09 07:36:46 +0200

    updated api

--- app/api/db.py
+++ app/api/db.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from dictalchemy import DictableModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 db_uri = "sqlite:///movie.db"
 engine = create_engine(db_uri, connect_args={'check_same_thread':False})
